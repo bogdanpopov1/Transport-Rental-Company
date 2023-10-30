f49135a2f445438291ce89059087e3bc5349d8d0
Bogdan <bogdan_popov17@mail.ru>
2023-10-30 15:31:51 +0300

    Создание 4-х моделей автомобилей

--- Company/Program.cs
+++ Company/Program.cs
@@ -4,4 +4,9 @@ List<Transport> transports = new List<Transport>();
 
 Boat boat001 = new Boat(40, "gray", "boat001", "B", false, 0, 70);
 Boat boat002 = new Boat(80, "green", "boat002", "B", false, 0, 90);
-Boat boat003 = new Boat(15, "blue", "boat003", "A", false, 0, 7);
+Boat boat003 = new Boat(15, "blue", "boat003", "A", true, 0, 7);
+
+Car car100 = new Car(170, "black", "car100", "B", false, 4, 200);
+Car car101 = new Car(200, "pink", "car101", "B", true, 4, 350);
+Car car102 = new Car(240, "purple", "car102", "B", true, 4, 400);
+Car car103 = new Car(200, "gray", "car103", "B", false, 6, 500);
