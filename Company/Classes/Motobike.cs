6a26aef269eb49c98ff7d02cdd0ebf406068edac
Пользователь <@KFM>
2023-10-30 11:06:15 +0300

    Добавьте файлы проекта.

---
+++ Company/Classes/Motobike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Classes
+{
+    internal class Motobike : Transport
+    {
+        public override int Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Colour { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string CategoryOfRights { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override bool IsRented { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int NumberOfWheels { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override double Motor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Motobike(int speed, string colour, string model, string categoryofrights, bool isrented, int numberofwheels, double motor) : base(speed, colour, model, categoryofrights, isrented, numberofwheels, motor)
+        {
+        }
+
+        public override void Rent()
+        {
+            IsRented = true;
+        }
+    }
+}
---
+++ Company/Classes/Transport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Company.Classes
+{
+    abstract class Transport
+    {
+        public abstract int Speed { get; set; }
+        public abstract string Colour { get; set; }
+        public abstract string Model { get; set; }
+        public abstract string CategoryOfRights { get; set; }
+        public abstract bool IsRented { get; set; }
+        public abstract int NumberOfWheels { get; set; }
+        public abstract double Motor { get; set; }
+
+        protected Transport(int speed, string colour, string model, string categoryofrights, bool isrented, int numberofwheels, double motor)
+        {
+            Speed = speed;
+            Colour = colour;
+            Model = model;
+            CategoryOfRights = categoryofrights;
+            IsRented = isrented;
+            NumberOfWheels = numberofwheels;
+            Motor = motor;
+        }
+        public abstract void Rent();
+    }
+}
