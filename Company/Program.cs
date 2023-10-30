b6f36a4ff2ce64b1098e4435d6af08fe6049c571
Bogdan <bogdan_popov17@mail.ru>
2023-10-30 15:48:20 +0300

    Создание 5-ти моделей мотоциклов

--- Company/Program.cs
+++ Company/Program.cs
@@ -14,3 +14,9 @@ Car car103 = new Car(200, "gray", "car103", "B", false, 6, 500);
 Bike bike200 = new Bike(30, "yellow", "bike200", "-", true, 2, 0);
 Bike bike201 = new Bike(10, "pink", "bike201", "-", false, 4, 0);
 Bike bike202 = new Bike(20, "white", "bike202", "-", true, 2, 0);
+
+Motorbike motorbike300 = new Motorbike(90, "orange", "motorbike300", "A1", false, 2, 50.5);
+Motorbike motorbike301 = new Motorbike(120, "ice blue", "motorbike301", "A", true, 2, 77.7);
+Motorbike motorbike302 = new Motorbike(130, "silver", "motorbike302", "A", false, 2, 80.2);
+Motorbike motorbike303 = new Motorbike(70, "brown", "motorbike303", "A1", true, 3, 40.2);
+Motorbike motorbike304 = new Motorbike(143, "soft pink", "motorbike304", "A", false, 2, 100);
