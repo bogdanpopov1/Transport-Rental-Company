dead23c2deb5d08627b60b4755946da492dba1d6
Bogdan <bogdan_popov17@mail.ru>
2023-10-30 15:24:19 +0300

    Создание списка и 3-х моделей лодок

--- Company/Program.cs
+++ Company/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using Company.Classes;
+
+List<Transport> transports = new List<Transport>();
+
+Boat boat001 = new Boat(40, "gray", "boat001", "B", false, 0, 70);
+Boat boat002 = new Boat(80, "green", "boat002", "B", false, 0, 90);
+Boat boat003 = new Boat(15, "blue", "boat003", "A", false, 0, 7);
